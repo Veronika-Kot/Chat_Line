c34a0f8824a7bb6b21a7919730202b70de0823bd
Veronika-Kot <Veronika-Kot@Veronikas-MacBook-Pro.local>
2016-02-11 21:00:18 -0800

    First Commit

---
+++ Chat_Line/LoginViewController.swift
@@ -0,0 +1,63 @@
+//
+//  LoginViewController.swift
+//  Chat_Line
+//
+//  Created by Veronika Kotckovich on 2/11/16.
+//  Copyright © 2016 Veronika Kotckovich. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class LoginViewController: UIViewController {
+  
+    @IBOutlet weak var emailText: UITextField!
+
+    @IBOutlet weak var pswdText: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func OnSignUp(sender: AnyObject) {
+        let user = PFUser()
+        user.username = "myUsername"
+        user.password = "myPassword"
+        user.email = "email@example.com"
+        // other fields can be set just like with PFObject
+        user["phone"] = "415-392-0202"
+        
+        user.signUpInBackgroundWithBlock {
+            (succeeded: Bool, error: NSError?) -> Void in
+            if let error = error {
+                let errorString = error.userInfo["error"] as? NSString
+                // Show the errorString somewhere and let the user try again.
+            } else {
+                // Hooray! Let them use the app now.
+            }
+        }
+
+    
+    }
+
+    @IBAction func OnLogin(sender: AnyObject) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Pods/Target Support Files/Pods-Chat_Line/Pods-Chat_Line-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_Chat_LineVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Chat_LineVersionString[];
+
